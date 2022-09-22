def7ff855969230481f91b0ff437ebbc345d0660
shotak <fk.user13@gmail.com>
2022-09-22 14:47:17 +0400

    finished with all UI and Functionality

--- app.js
+++ app.js
@@ -1,27 +1,107 @@
 "use strict";
-const container = document.querySelector(".container");
-const button = document.querySelector(".btn");
+const DEFAULT_COLOR = "#333";
+const DEFAULT_MODE = "color";
+const DEFAULT_SIZE = 16;
 
-//btn metods
-button.addEventListener("click", () => {
-  const out = Number(prompt("Enter grid size max 100x100"));
+let currentColor = DEFAULT_COLOR;
+let currentMode = DEFAULT_MODE;
+let currentSize = DEFAULT_SIZE;
 
-  if (out >= 100) {
-    return alert("longer then 100 or invalid input ");
-  }
-  if (out == "") {
-    return alert("longer then 100 or invalid input ");
-  }
-  console.log(typeof out);
-  return makeRows(out, out);
-});
+const setCurrentColor = (newColor) => (currentColor = newColor);
 
-function makeRows(rows, cols) {
-  container.style.setProperty("--grid-rows", rows);
-  container.style.setProperty("--grid-cols", cols);
-  for (let c = 0; c < rows * cols; c++) {
-    let cell = document.createElement("button");
-    // cell.innerText = c + 1;
-    container.appendChild(cell).className = "grid-item";
+const setCurrentMode = (newMode) => {
+  activateButton(newMode);
+  currentMode = newMode;
+};
+
+const setCurrentSize = (newSize) => (currentSize = newSize);
+
+const colorPicker = document.getElementById("colorPicker");
+const colorBtn = document.getElementById("colorBtn");
+const rainbowBtn = document.getElementById("rainbowBtn");
+const eraserBtn = document.getElementById("eraserBtn");
+const clearBtn = document.getElementById("clearBtn");
+const sizeValue = document.getElementById("sizeValue");
+const sizeSlider = document.getElementById("sizeSlider");
+const grid = document.getElementById("grid");
+
+colorPicker.oninput = (e) => setCurrentColor(e.target.value);
+colorBtn.onclick = () => setCurrentMode("color");
+rainbowBtn.onclick = () => setCurrentMode("rainbow");
+eraserBtn.onclick = () => setCurrentMode("eraser");
+clearBtn.onclick = () => reloadGrid();
+sizeSlider.onmousemove = (e) => updateSizeValue(e.target.value);
+sizeSlider.onchange = (e) => changeSize(e.target.value);
+
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+function changeSize(value) {
+  setCurrentSize(value);
+  updateSizeValue(value);
+  reloadGrid();
+}
+
+function updateSizeValue(value) {
+  sizeValue.innerHTML = `${value} x ${value}`;
+}
+
+function reloadGrid() {
+  clearGrid();
+  setupGrid(currentSize);
+}
+
+function clearGrid() {
+  grid.innerText = "";
+}
+
+function setupGrid(size) {
+  grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  for (let i = 0; i < size * size; i++) {
+    const gridElement = document.createElement("div");
+    gridElement.classList.add("grid-element");
+    gridElement.addEventListener("mouseover", changeColor);
+    gridElement.addEventListener("mousedown", changeColor);
+    grid.appendChild(gridElement);
   }
 }
+
+function changeColor(e) {
+  if (e.type === "mouseover" && !mouseDown) return;
+  if (currentMode === "rainbow") {
+    const randomR = Math.floor(Math.random() * 256);
+    const randomG = Math.floor(Math.random() * 256);
+    const randomB = Math.floor(Math.random() * 256);
+    e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+  } else if (currentMode === "color") {
+    e.target.style.backgroundColor = currentColor;
+  } else if (currentMode === "eraser") {
+    e.target.style.backgroundColor = "#fefefe";
+  }
+}
+
+function activateButton(newMode) {
+  if (currentMode === "rainbow") {
+    rainbowBtn.classList.remove("active");
+  } else if (currentMode === "color") {
+    colorBtn.classList.remove("active");
+  } else if (currentMode === "eraser") {
+    eraserBtn.classList.remove("active");
+  }
+
+  if (newMode === "rainbow") {
+    rainbowBtn.classList.add("active");
+  } else if (newMode === "color") {
+    colorBtn.classList.add("active");
+  } else if (newMode === "eraser") {
+    eraserBtn.classList.add("active");
+  }
+}
+
+window.onload = () => {
+  setupGrid(DEFAULT_SIZE);
+  activateButton(DEFAULT_MODE);
+};
