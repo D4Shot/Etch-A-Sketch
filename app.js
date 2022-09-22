6b04fa20a775a57b839c2ed2548804b7c9db646a
shotak <fk.user13@gmail.com>
2022-09-22 13:45:28 +0400

    added some html  markup

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 "use strict";
 const container = document.querySelector(".container");
+const button = document.querySelector(".btn");
+
+//btn metods
+button.addEventListener("click", () => {
+  const out = Number(prompt("Enter grid size max 100x100"));
+
+  if (out >= 100) {
+    return alert("longer then 100 or invalid input ");
+  }
+  if (out == "") {
+    return alert("longer then 100 or invalid input ");
+  }
+  console.log(typeof out);
+  return makeRows(out, out);
+});
 
 function makeRows(rows, cols) {
   container.style.setProperty("--grid-rows", rows);
   container.style.setProperty("--grid-cols", cols);
   for (let c = 0; c < rows * cols; c++) {
-    let cell = document.createElement("div");
-    cell.innerText = c + 1;
+    let cell = document.createElement("button");
+    // cell.innerText = c + 1;
     container.appendChild(cell).className = "grid-item";
   }
 }
-
-makeRows(16, 16);
